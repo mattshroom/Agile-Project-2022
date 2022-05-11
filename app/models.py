6f1d2d4ffcd67e798039f1588bb36ffebf443a5e
camtheboom <101784910+camtheboom@users.noreply.github.com>
2022-05-11 16:17:17 +0800

    This branch is to get registrations and login databases working

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from datetime import datetime
+from signal import default_int_handler
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from app import login
 
 class User(db.Model):
     
@@ -7,10 +10,32 @@ class User(db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(128), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+    # def is_active(self):
+    #     """True, as all users are active."""
+    #     return True
+    # def get_id(self):
+    #     """Return the email address to satisfy Flask-Login's requirements."""
+    #     return self.email
+
+    # def is_authenticated(self):
+    #     """Return True if the user is authenticated."""
+    #     return self.authenticated
+
+    # def is_anonymous(self):
+    #     """False, as anonymous users aren't supported."""
+    #     return False
 
 class Result(db.Model):
     result_id = db.Column(db.Integer, primary_key=True)
@@ -31,3 +56,25 @@ class Logo(db.Model):
 
     def __repr__(self):
         return f'<Logo {self.logoname}>'
+
+from flask_login import UserMixin
+
+class User(UserMixin, db.Model):
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return self.authenticated
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
 from flask import render_template, url_for, flash, redirect
 from app import app
 from app.forms import LoginForm
-
+from flask_login import current_user, login_user
+from app.models import User
+from flask_login import logout_user
+from flask_login import login_required
+from flask import request
+from werkzeug.urls import url_parse
+from app.forms import RegistrationForm
+from app import db
 
 @app.route('/')
 @app.route('/index')
+@login_required
 
 def index():
     # User login JSON sample
@@ -22,21 +30,96 @@ def index():
         }
     ]
 
-    return render_template("index.html",title="Home",user=user, posts=posts)
+    return render_template("index.html", title='Home Page', posts=posts)
 
 @app.route('/button')
 def button():
     return render_template("button.html", title="Button!")
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.filter_by(username=form.username.data).first()
+#         if user is None or not user.check_password(form.password.data):
+#             flash('Invalid username or password')
+#             return redirect(url_for('login'))
+#         login_user(user, remember=form.remember_me.data)
+#         next_page = request.args.get('next')
+#         if not next_page or url_parse(next_page).netloc != '':
+#             next_page = url_for('index')
+#         return redirect(url_for('index'))
     
-    form = LoginForm()
+#     return render_template('login.html', title='Sign In', form=form)
+
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     return redirect(url_for('index'))
+
+# @app.route('/register', methods=['GET', 'POST'])
+# def register():
+#     if current_user.is_authenticated:
+#         return redirect(url_for('index'))
+#     form = RegistrationForm()
+#     if form.validate_on_submit():
+#         user = User(username=form.username.data, email=form.email.data)
+#         user.set_password(form.password.data)
+#         db.session.add(user)
+#         db.session.commit()
+#         flash('Congratulations, you are now a registered user!')
+#         return redirect(url_for('login'))
+#     return render_template('register.html', title='Register', form=form)
+
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
     
-    if form.validate_on_submit():
+#     form = LoginForm()
+    
+#     if form.validate_on_submit():
         
-        flash('login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+#         flash('login requested for user {}, remember_me={}'.format(
+#             form.username.data, form.remember_me.data))
+#         return redirect(url_for('index'))
     
-    return render_template('login.html', title='sign in', form=form)
+#     return render_template('login.html', title='sign in', form=form)
+
+# @login_manager.user_loader
+# def user_loader(user):
+#     """Given *user_id*, return the associated User object.
+
+#     :param unicode user_id: user_id (email) user to retrieve
+
+#     """
+#     return User.query.get(user.id)
+
+
+# def login():
+#     """For GET requests, display the login form. 
+#     For POSTS, login the current user by processing the form.
+
+#     """
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         user = User.query.get(form.email.data)
+#         if user:
+#             if bcrypt.check_password_hash(user.password, form.password.data):
+#                 user.authenticated = True
+#                 db.session.add(user)
+#                 db.session.commit()
+#                 login_user(user, remember=True)
+#                 return redirect(url_for("bull.reports"))
+#     return render_template("login.html", form=form)
+
+# @app.route("/logout", methods=["GET"])
+# @login_required
+# def logout():
+#     """Logout the current user."""
+#     user = current_user
+#     user.authenticated = False
+#     db.session.add(user)
+#     db.session.commit()
+#     logout_user()
+#     return render_template("logout.html")
