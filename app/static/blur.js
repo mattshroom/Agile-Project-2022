a182e9c2faf54aa5662115988f1c555d6a38c2cd
Matt Ong <matthaeusong@gmail.com>
2022-05-12 11:17:58 +0800

    clearing up excess code

--- app/static/blur.js
+++ app/static/blur.js
@@ -4,7 +4,6 @@ var toggle = false;
 var solved = false;
 
 var logo = document.getElementById('logo');
-// img.addEventListener('onload',blurTime);
 
 function blurOnStart(){
     logo.classList.add('startingBlur')
@@ -12,22 +11,6 @@ function blurOnStart(){
 }
 
 function unblurStart(){
-    // document.getElementById("image").style= "blur(12px)";
     logo.classList.add('unBlur');
     logo.classList.toggle('startingBlur')
-}
-
-function blurImageClick(){
-    toggle = !toggle;
-    
-    // if(toggle){
-    //     document.getElementById("image").style.filter = "blur(25px)";
-    // }
-    // else{
-    //     document.getElementById("image").style.filter = "blur(0px)";
-    // }
-    
-
-
-
 }
