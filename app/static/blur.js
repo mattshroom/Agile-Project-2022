b312a13e6d6ee3293d60c1dd2159fb08ccfe2ca5
camtheboom <101784910+camtheboom@users.noreply.github.com>
2022-05-16 20:31:16 +0800

    Now able to make get requests to the json file and post photos to website

--- app/static/blur.js
+++ app/static/blur.js
@@ -1,26 +1,36 @@
 var timer = 300000;
 var toggle = false;
 var solved = false;
-var startTime, stopTime, timeDiff, score;
-
-var logo = document.getElementById('logo');
+var startTime, stopTime, timeDiff, guessNum, score;
+let id = Math.floor((Math.random() * 19) + 1);
 
 // function blurOnStart(){
 //     logo.classList.add('startingBlur')
 // }
 
 function unblurStart(){
+
+    var logo1 = document.querySelector("#imgID");
     if(!toggle){
         toggle = true;
         startTime = performance.now();
-        logo.classList.add('unBlur');
-        logo.classList.toggle('startingBlur');
+        console.log(startTime)
+        console.log("Starting to unblur!")
+        logo1.classList.add('unBlur');
+        logo1.classList.toggle('startingBlur');
+        
     }
-    if(solved){
+
+    console.log("Waiting...")
+
+    if(performance.now() - startTime != 20000){ // in ms
+        guessNum = 2;
+        console.log("Time's Up!")
         stopTime = performance.now();
         timeDiff = stopTime - startTime;
-        score = calculateScore(guessNum, timeDiff);
-
+        score = setInterval(calculateScore,1000,guessNum, timeDiff);
+        console.log("Score is: ", score)
+        solved = true;
     }
 }
 
@@ -29,7 +39,18 @@ function calculateScore(guessNum, timeDiff){
     multiplier = (timeDiff * guessNum) / 15;
     var score = baseScore - multiplier;
     return score;
-
 }
 
+function jsonImage() {
+   fetch('../static/logo_json.json')
+      .then(response => response.json())
+      .then(data => {
+         //document.querySelector("#debug").innerText = data.logos[0].logo_link
+         var image = data.logos[id].logo_link;
+         document.getElementById("imgID").src= image;
+      })
+}
 
+window.onload = function() {
+    jsonImage()
+}
