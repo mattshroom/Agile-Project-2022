35d8bee3729b634c8b7c973f330e420e6ddb4d35
Nicholas Chua <101857882+nic-chua@users.noreply.github.com>
2022-05-23 01:01:35 +0800

    62 fixing profile icon (#64)
    * fixed styles
    
    * cheeseburger 2
    
    * plz

--- app/routes.py
+++ app/routes.py
@@ -36,6 +36,10 @@ def index():
         {
             'author':{'username':'Lachy'},
             'body':'Compete with your friends to get the best score!'
+        },
+        {
+            'author':{'username':'Matt'},
+            'body':'Click on the "Blurgo" at the top to play!'
         }
     ]
 
